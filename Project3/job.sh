6af383c4a49e7f209a26b666d84f9a3249765dd3
Mingzhi Hu <115661115+mhu3@users.noreply.github.com>
2024-10-07 14:57:38 -0400

    Add files via upload

---
+++ Project3/job.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+#SBATCH -A cs525
+#SBATCH -p academic
+#SBATCH -N 1
+#SBATCH -c 8
+#SBATCH --gres=gpu:1
+#SBATCH -C A100
+#SBATCH -t 24:00:00
+#SBATCH --mem 12G
+#SBATCH --job-name="P3"
+
+source activate pt
+python main.py --train_dqn
